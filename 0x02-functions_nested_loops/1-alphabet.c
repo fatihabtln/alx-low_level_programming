f46ddff48356b521205972134f3103e238526325
fatiha boutalount <fatiha.boutalount@gmail.com>
2023-04-25 18:13:29 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 
-	/**
-	 * print_alphabet - print all alphabet in lowercase
-	 */
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
 
-	/*
-	 *print_alphabet - function for print alphabet in lower case
-	*/
+/*
+*print_alphabet - function for print alphabet in lower case
+*/
 
-	void print_alphabet(void)
-	{
+void print_alphabet(void)
+{
 
