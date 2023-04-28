56895e87a0d709a180742bade653e535d98e6d7e
fatiha boutalount <fatiha.boutalount@gmail.com>
2023-04-28 16:58:43 -0700

    fira

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,19 @@
 /**
 * _isupper - checks for uppercase character
 * @c: input
+*
 * Return: 1 if c is uppercase, 0 otherwise
 */
 
 int _isupper(int c)
+
 {
-if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 	return (1);
 	}
 
-else
+	else
 	{
 	return (0);
 	}
