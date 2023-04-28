b0971f151b1173350946a4a5be846ea9bd311d2f
fatiha boutalount <fatiha.boutalount@gmail.com>
2023-04-27 18:19:39 -0700

    first

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 int i;
-i = 0;
 
 printf("Infinite loop incoming :(\n");
 
-while (i < 10)
+i = 0;
+/**while (i < 10)
 {
 putchar(i);
-}
+}*/
 
 printf("Infinite loop avoided! \\o/\n");
 
